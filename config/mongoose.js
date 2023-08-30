8b0acbeb9a3342de227cb577c19a6baa132d5147
avishala8 <abhishekvishala@gmail.com>
2023-08-30 17:58:42 +0530

    added config.env,removing localhost DB

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/rippleApp_dev");
-const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to the DB"));
-db.once("open", function () {
-  console.log("Successfully connected to the Database");
-});
-exports.db = db;
+const dotenv = require("dotenv");
+// const app = express();
+
+dotenv.config({ path: "./config.env" });
+const db = process.env.DATA;
+mongoose
+  .connect(db, {
+    ///returns a promise and should be handled
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  });
+
+// mongoose.connect("mongodb://localhost/rippleApp_dev");
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "Error connecting to the DB"));
+// db.once("open", function () {
+//   console.log("Successfully connected to the Database");
+// });
+// exports.db = db;
